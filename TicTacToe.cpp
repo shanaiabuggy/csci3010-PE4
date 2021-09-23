278abbbf9b64082871bad5993e6bc50281733ee1
Shanaia <shanaia.buggy@gmail.com>
2021-09-23 15:47:05 -0600

    added funcs

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
 
+int * CreateBoard() {
+  int *ptr;
+  int board[3][3];
+  ptr =  &board[3][3];
+  return ptr;
+}
+
+int main(){
+  int *b;
+  b = CreateBoard();
+
+  return 0;
+}
