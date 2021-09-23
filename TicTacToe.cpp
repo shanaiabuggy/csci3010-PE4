1768f59274d223658e49c3fd89eecd7cee782dc2
Shanaia <shanaia.buggy@gmail.com>
2021-09-23 15:37:37 -0600

    second commit

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -1,16 +1,1 @@
-#include <iostream>
 
-int * CreateBoard() {
-  int *ptr;
-  int board[3][3];
-  ptr =  &board[3][3];
-  return ptr;
-}
-
-int main(){
-  int *b;
-  b = CreateBoard();
-
-  return 0;
-  // this is a comment
-}
