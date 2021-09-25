728f3408ae0a8383a857eb43ed497d10a776b15d
MichaelZigray <59930528+MichaelZigray@users.noreply.github.com>
2021-09-24 23:14:02 -0600

    My changes for Step 9

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 
-int * CreateBoard() {
-  int *ptr;
-  int board[3][3];
+
+char * CreateBoard() {
+  char *ptr;
+  char board[3][3];
   ptr =  &board[3][3];
   return ptr;
 }
 
-void DisplayBoard(int * board) {
+void DisplayBoard(char * board) {
   for(int i = 0; i < 3; i++) {
     std::string line = "";
     line += std::to_string(board[i*3]) + " ";
@@ -17,8 +18,32 @@ void DisplayBoard(int * board) {
   }
 }
 
+struct Point {
+  Point(int xVal, int yVal) {
+    x = xVal;
+    y = yVal;
+  }
+  int x;
+  int y;
+};
+
+Point GetPlayerChoice() {
+  std::cout << "Enter a row (0-2):" << std::endl;
+  int x;
+  std::cin >> x;
+  int y;
+  std::cout << "Enter a col (0-2):" << std::endl;
+  std::cin >> y;
+
+  return Point(x, y);
+}
+
+void PlaceMarker(Point p, char * board, char player) {
+  board[p.x + 3*p.y] = player;
+}
+
 int main(){
-  int *b;
+  char *b;
   b = CreateBoard();
   DisplayBoard(b);
   return 0;
